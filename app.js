439ff6dcfcb4e22e89f1dfbd77c2ef952476d38e
Mauro Corvalan <maurocorvaa@gmail.com>
2023-10-16 22:12:10 -0300

    Agregando el update
    Agrego busqueda de pacientes, update en caso de existir y en caso de no crearse uno nuevo.

--- app.js
+++ app.js
@@ -1,48 +1,73 @@
 const express = require('express');
 const app = express();
 const sequelize = require('./config/database');
-const pacienteRuta = require('./routes/pacienteRuta'); 
+const pacienteRuta = require('./routes/pacienteRuta');
 const Paciente = require('./models/paciente');
+
+app.set('view engine', 'pug');
+app.set('views', __dirname + '/views');
+
 app.use(express.static(__dirname + '/public'));
 app.use(express.urlencoded({ extended: true }));
-app.use('/', pacienteRuta); 
+app.use('/', pacienteRuta);
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + '/views/ingresarPaciente.html');
+    res.render('busquedaPaciente'); // Ruta de búsqueda de paciente
 });
 
-// Ruta para procesar el formulario
 app.post('/guardar-paciente', async (req, res) => {
     try {
-        // Obtiene los datos del formulario
-        const { nombre, apellido, dni,  email, telefono, fecha_nacimiento, genero, embarazo,
-            diagnostico} = req.body;
-
-        // Guarda los datos en la base de datos
-        await Paciente.create({
+        const {
+            dni, // Obtén el campo dni del formulario
             nombre,
             apellido,
-            dni,
             email,
             telefono,
             fecha_nacimiento,
             genero,
             embarazo,
-            diagnostico
-        });
+            diagnostico,
+        } = req.body;
 
-        // Muestra un mensaje por consola
-        console.log('Datos del paciente guardados con éxito:', nombre, apellido, dni);
+        const existingPaciente = await Paciente.findOne({ where: { dni } });
 
-        // Redirige o muestra un mensaje de éxito
-        res.redirect('/formulario'); // Puedes redirigir a donde desees o mostrar un mensaje de éxito
+        if (existingPaciente) {
+            // Si el paciente existe, actualiza los datos
+            await existingPaciente.update({
+                nombre,
+                apellido,
+                email,
+                telefono,
+                fecha_nacimiento,
+                genero,
+                embarazo,
+                diagnostico,
+            });
+            console.log('Datos del paciente actualizados con éxito:', nombre, apellido, dni);
+        } else {
+            // Si no se encuentra un paciente con el DNI, crea uno nuevo
+            await Paciente.create({
+                nombre,
+                apellido,
+                dni,
+                email,
+                telefono,
+                fecha_nacimiento,
+                genero,
+                embarazo,
+                diagnostico,
+            });
+            console.log('Datos del paciente guardados con éxito:', nombre, apellido, dni);
+        }
+
+        res.redirect('/');
     } catch (error) {
         console.error(error);
         res.status(500).send('Error al guardar el paciente en la base de datos.');
     }
 });
 
-sequelize.sync() // Sincroniza los modelos con la base de datos
+sequelize.sync()
     .then(() => {
         app.listen(3000, () => {
             console.log('Servidor en ejecución en el puerto 3000');
--- routes/pacienteRuta.js
+++ routes/pacienteRuta.js
@@ -1,26 +1,64 @@
 const express = require('express');
 const router = express.Router();
-const paciente = require('../models/paciente'); // Importa el modelo de paciente
+const Paciente = require('../models/paciente');
+
+router.get('/ingresar-paciente', (req, res) => {
+    res.render('ingresarPaciente', { paciente: null }); // Renderiza el formulario de ingreso de pacientes
+});
+
+router.post('/buscar-paciente', async (req, res) => {
+    const dni = req.body.dni; // Obtiene el DNI ingresado en el formulario
+
+    try {
+        const paciente = await Paciente.findOne({ where: { dni } });
+
+        if (paciente) {
+            // Paciente encontrado, muestra el formulario con los campos llenos
+            res.render('ingresarPaciente', { paciente });
+        } else {
+            // Paciente no encontrado, muestra el mismo formulario vacío
+            res.render('ingresarPaciente', { paciente: null });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al buscar paciente por DNI.');
+    }
+});
 
 router.post('/guardar-paciente', async (req, res) => {
     try {
-        const { nombre, apellido, dni, email, telefono, direccion, fecha_nacimiento, genero, embarazo,diagnostico } = req.body;
+        const { dni, nombre, apellido, email, telefono, fecha_nacimiento, genero, embarazo, diagnostico } = req.body;
 
-        await paciente.create({
-            nombre,
-            apellido,
-            dni,
-            email,
-            telefono,
-            direccion,
-            fecha_nacimiento,
-            genero,
-            embarazo,
-            diagnostico,
-        });
+        const existingPaciente = await Paciente.findOne({ where: { dni } });
 
-        res.send('Paciente guardado con éxito.');
-        console.log(paciente.id_paciente)
+        if (existingPaciente) {
+            await existingPaciente.update({
+                nombre,
+                apellido,
+                email,
+                telefono,
+                fecha_nacimiento,
+                genero,
+                embarazo,
+                diagnostico,
+            });
+            console.log('Datos del paciente actualizados con éxito:', nombre, apellido, dni);
+        } else {
+            await Paciente.create({
+                nombre,
+                apellido,
+                dni,
+                email,
+                telefono,
+                fecha_nacimiento,
+                genero,
+                embarazo,
+                diagnostico,
+            });
+            console.log('Datos del paciente guardados con éxito:', nombre, apellido, dni);
+        }
+
+        res.redirect('/');
     } catch (error) {
         console.error(error);
         res.status(500).send('Error al guardar el paciente en la base de datos.');
