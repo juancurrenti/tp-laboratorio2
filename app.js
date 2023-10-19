ffd2adfacdcdd289519d484bb7b1f6a7bd218633
Mauro Corvalan <maurocorvaa@gmail.com>
2023-10-19 17:31:38 -0300

    cambios
    Se le agrega una lista, en caso de que coincidan los apellidos, se muestren todos los que hay con ese apellido

--- app.js
+++ app.js
@@ -4,25 +4,33 @@ const sequelize = require('./config/database');
 const pacienteRuta = require('./routes/pacienteRuta');
 const Paciente = require('./models/paciente');
 const path = require('path');
+
+// Configuración de la vista
 app.set('view engine', 'pug');
 app.set('views', __dirname + '/views');
 
+// Middleware para servir archivos estáticos desde la carpeta '/public'
 app.use('/public', express.static(path.join(__dirname, 'public'), {
     setHeaders: (res, path, stat) => {
-      res.set('Content-Type', 'text/css');
+      res.set('Content-Type', 'text/css'); // Configura el tipo de contenido para archivos CSS
     },
-  }));
+}));
+
 app.use(express.urlencoded({ extended: true }));
+
+// Middleware para manejar rutas relacionadas con pacientes
 app.use('/', pacienteRuta);
 
+
 app.get('/', (req, res) => {
-    res.render('busquedaPaciente'); // Ruta de búsqueda de paciente
+    res.render('busquedaPaciente'); 
 });
 
+// Controlador para la ruta de guardar paciente
 app.post('/guardar-paciente', async (req, res) => {
     try {
         const {
-            dni, // Obtén el campo dni del formulario
+            dni, 
             nombre,
             apellido,
             email,
@@ -32,8 +40,6 @@ app.post('/guardar-paciente', async (req, res) => {
             embarazo,
             diagnostico,
         } = req.body;
-
-        // Obtiene la fecha actual para la fecha_registro
         const fecha_registro = new Date();
 
         const existingPaciente = await Paciente.findOne({ where: { dni } });
@@ -68,13 +74,14 @@ app.post('/guardar-paciente', async (req, res) => {
             console.log('Datos del paciente guardados con éxito:', nombre, apellido, dni);
         }
 
-        res.redirect('/');
+        res.redirect('/'); // Redirige de vuelta a la página principal
     } catch (error) {
         console.error(error);
         res.status(500).send('Error al guardar el paciente en la base de datos.');
     }
 });
 
+// Sincronización de modelos con la base de datos y arranque del servidor en el puerto 3000
 sequelize.sync()
     .then(() => {
         app.listen(3000, () => {
--- routes/pacienteRuta.js
+++ routes/pacienteRuta.js
@@ -7,28 +7,66 @@ router.get('/ingresar-paciente', (req, res) => {
 });
 
 router.post('/buscar-paciente', async (req, res) => {
-    const searchType = req.body.searchType; // Obtiene el tipo de búsqueda seleccionado (DNI, apellido o email)
-    const searchTerm = req.body.searchTerm; // Obtiene el valor de búsqueda ingresado
+    const searchType = req.body.searchType;
+    const searchTerm = req.body.searchTerm;
 
     try {
         const whereCondition = {};
 
-        // Establece la condición de búsqueda según el tipo seleccionado
         if (searchType === 'dni') {
             whereCondition.dni = searchTerm;
         } else if (searchType === 'apellido') {
-            whereCondition.apellido = searchTerm;
+            // Cambio: Buscar todos los pacientes con el mismo apellido
+            const pacientes = await Paciente.findAll({ where: { apellido: searchTerm } });
+
+            if (pacientes.length > 1) {
+                // Si hay múltiples pacientes con el mismo apellido, mostrar una lista para seleccionar
+                res.render('seleccionarPaciente', { pacientes, searchTerm, searchType });
+            } else if (pacientes.length === 1) {
+                // Si se encuentra un paciente, renderizar el formulario con los campos llenos
+                res.render('ingresarPaciente', { paciente: pacientes[0], mensaje: 'Paciente encontrado:' });
+            } else {
+                // Si no se encuentra un paciente, mostrar un mensaje de error
+                res.render('ingresarPaciente', { paciente: null, mensaje: 'Paciente no encontrado. Ingrese los datos del paciente.' });
+            }
+            return; // Importante: sal del controlador después de renderizar la lista de pacientes
         } else if (searchType === 'email') {
             whereCondition.email = searchTerm;
         }
 
         const paciente = await Paciente.findOne({ where: whereCondition });
 
-        // Renderiza el formulario con los campos llenos si se encuentra un paciente, o vacío si no se encuentra
-        res.render('ingresarPaciente', { paciente, mensaje: paciente ? 'Paciente encontrado: ' : 'Paciente no encontrado. Ingrese los datos del paciente.' });
+        if (paciente) {
+            // Si se encuentra un paciente, renderizar el formulario con los campos llenos
+            res.render('ingresarPaciente', { paciente, mensaje: 'Paciente encontrado:' });
+        } else {
+            // Si no se encuentra un paciente, mostrar un mensaje de error
+            res.render('ingresarPaciente', { paciente: null, mensaje: 'Paciente no encontrado. Ingrese los datos del paciente.' });
+        }
     } catch (error) {
         console.error(error);
         res.status(500).send('Error al buscar paciente por DNI, apellido o email.');
+    }
+});
+
+// Controlador para seleccionar un paciente de la lista
+router.get('/editar-paciente/:id', async (req, res) => {
+    const { id } = req.params;
+    const { searchTerm, searchType } = req.query;
+
+    try {
+        const paciente = await Paciente.findByPk(id);
+
+        if (paciente) {
+            // Renderiza el formulario con los campos llenos y muestra el mensaje
+            res.render('ingresarPaciente', { paciente, mensaje: 'Paciente seleccionado:' });
+        } else {
+            // Si no se encuentra un paciente, muestra un mensaje de error
+            res.render('ingresarPaciente', { paciente: null, mensaje: 'Paciente no encontrado. Ingrese los datos del paciente.' });
+        }
+    } catch (error) {
+        console.error(error);
+        res.status(500).send('Error al seleccionar paciente para edición.');
     }
 });
 
