4c311e2c0a95d2e170256456f03c6dd8a6ae8d66
Mauro Corvalan <maurocorvaa@gmail.com>
2023-10-25 23:51:38 -0300

    a

--- app.js
+++ app.js
@@ -51,7 +51,7 @@ app.use('/examen', examenRuta);
 app.use('/determinacion', determinacionesRuta);
 // Middleware para manejar rutas relacionadas con valores de referencia
 app.use('/valoresreferencia', valoresRefRuta);
-app.use('/generar-orden', OrdenesTrabajoRuta);
+app.use('/generacion-orden', OrdenesTrabajoRuta);
 // Ruta para mostrar la vista de generación de orden
 app.get('/generar-orden/:id_paciente/:nombre/:apellido/:dni', (req, res) => {
     const tiposMuestra = [
--- routes/ordenes_trabajoRuta.js
+++ routes/ordenes_trabajoRuta.js
@@ -1,58 +1,26 @@
 const express = require('express');
 const router = express.Router();
-const OrdenTrabajo = require('../models/ordenes_trabajo');
-const Muestra = require('../models/muestra');
-const Examen = require('../models/examen');
+const OrdenTrabajo = require('../models/ordenes_trabajo'); 
+const Muestra = require('../models/muestra'); 
 
-router.get('/generar-orden/:id_paciente/:nombre/:apellido/:dni', async (req, res) => {
-  try {
-    // Obtén la lista de exámenes desde la base de datos
-    const tiposMuestra = [
-      { value: "sangre", label: "Sangre" },
-      { value: "orina", label: "Orina" },
-      { value: "heces", label: "Heces" },
-      { value: "liquidoCefaloraquideo", label: "Líquido Cefalorraquídeo" },
-      { value: "saliva", label: "Saliva" },
-      { value: "nasofaringea", label: "Secreción Nasofaríngea" }
-    ];
-    
-    const { id_paciente, nombre, apellido, dni } = req.params;
-
-    // Obtén la lista de exámenes desde la base de datos
-    const examenes = await Examen.findAll();
-    res.render('generarOrden', { tiposMuestra, id_paciente, nombre, apellido, dni, examenes });
-  } catch (error) {
-    console.error(error);
-    res.status(500).send('Error al obtener la lista de exámenes.');
-  }
-});
-
-router.post('/generacion-orden/:id_paciente', async (req, res) => {
+// Esta ruta maneja el envío del formulario y crea la orden de trabajo y muestras en la base de datos
+router.post('/generacion-orden', async (req, res) => {
   try {
     // Obtiene los datos del formulario
-    const { estado, examenes, tipos_muestra } = req.body;
-    const id_paciente = req.params.id_paciente;
-    
-    // Verifica si id_paciente es null
-    if (!id_paciente) {
-      return res.status(400).send('El valor de id_paciente es nulo o no válido.');
-    }
+    const { paciente, estado, examenes, tipos_muestra } = req.body;
 
     // Crea una nueva orden de trabajo
     const nuevaOrden = await OrdenTrabajo.create({
-      id_Paciente: id_paciente, // Corregir el nombre del campo
-      Fecha_Creacion: new Date(),
+      paciente,
       estado,
     });
 
     // Para cada tipo de muestra seleccionado en el formulario
     for (const tipoMuestra of tipos_muestra) {
       await Muestra.create({
-        id_Orden: nuevaOrden.id, // Corregir el nombre del campo
-        id_Paciente: id_paciente, // Corregir el nombre del campo
+        idOrdenTrabajo: nuevaOrden.id, 
         tipoMuestra,
-        Fecha_Recepcion: new Date(),
-        estadoMuestra: estado,
+        estadoMuestra: estado, 
       });
     }
 
